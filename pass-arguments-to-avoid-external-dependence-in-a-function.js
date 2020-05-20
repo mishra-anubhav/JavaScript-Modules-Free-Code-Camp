5e035c324dd994e715151cbf37f25076e89f421c
Anubhav Mishra <39110231+mishra-anubhav@users.noreply.github.com>
2020-05-20 17:24:45 +0530

    Add files via upload

---
+++ pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,10 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer (a) {
+
+return fixedValue +1
+  // Only change code above this line
+}
+incrementer(fixedValue);
