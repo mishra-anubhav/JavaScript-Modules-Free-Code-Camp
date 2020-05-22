8aec4ba12b827d99e3bd11f9207db5e1b26ae25f
Anubhav Mishra <39110231+mishra-anubhav@users.noreply.github.com>
2020-05-22 17:51:25 +0530

    Add files via upload

---
+++ return-part-of-an-array-using-the-slice-method.js
@@ -0,0 +1,8 @@
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+var newArray = anim.slice(beginSlice, endSlice);
+return newArray
+  // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
