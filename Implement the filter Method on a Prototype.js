dd4d5d376d8fd7b2e4a04b256e2dbbb54389698a
Anubhav Mishra <39110231+mishra-anubhav@users.noreply.github.com>
2020-09-17 21:23:57 +0530

    Update Implement the filter Method on a Prototype.js

--- Implement the filter Method on a Prototype.js
+++ Implement the filter Method on a Prototype.js
@@ -1,15 +1,13 @@
-// The global variable
+
 let s = [23, 65, 98, 5];
 
 Array.prototype.myFilter = function(callback){
-  // Only change code below this line
   let newArray = [];
   this.forEach(function(x) {
     if (callback(x) == true) {
       newArray.push(x);
     }
   });
-  // Only change code above this line
   return newArray;
 };
 
